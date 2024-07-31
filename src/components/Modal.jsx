07322fc850737077fd7c79b1ef7fb551e193d48d
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-31 16:37:30 +0700

    State & Conditional Content

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,12 @@
 import classes from "./Modal.module.css";
 
-function Modal({ children }) {
+function Modal({ children, onClose }) {
   return (
     <>
-      <div className={classes.backdrop}>
-        <dialog open className={classes.modal}>
-          {children}
-        </dialog>
-      </div>
+      <div className={classes.backdrop} onClick={onClose}></div>
+      <dialog open className={classes.modal}>
+        {children}
+      </dialog>
     </>
   );
 }
