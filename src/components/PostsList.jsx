b6e3672e9c96e5a57e9ed022f34c8e1cd18d5846
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-31 15:40:32 +0700

    Preparing the App For State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="This is body one" />
-      <Post author="Manuel" body="This is body two" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="This is body one" />
+        <Post author="Manuel" body="This is body two" />
+      </ul>
+    </>
   );
 }
 export default PostsList;
