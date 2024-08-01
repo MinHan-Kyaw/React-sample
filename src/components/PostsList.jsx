9ff2289ced120f7251c5abfe1fa3913413edb91b
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-08-01 23:45:58 +0700

    Refactoring Route Components & More Nesting

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-import NewPost from "./NewPost";
+import NewPost from "../routes/NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
-function PostsList({ isPosting, onStopPosting }) {
+function PostsList() {
   const [posts, setPosts] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -32,11 +32,6 @@ function PostsList({ isPosting, onStopPosting }) {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
       {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
