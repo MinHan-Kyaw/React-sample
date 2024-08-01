bd12b7d8c0935e14710aee3f06e777c638ef3949
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-08-01 15:53:07 +0700

    Handling Form Submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,44 +6,15 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  //   let modalContent;
-
-  //   if (modalIsVisible) {
-  //     modalContent = (
-  //       <Modal onClose={hideModalHandler}>
-  //         <NewPost
-  //           onBodyChange={bodyChangeHandler}
-  //           onAuthorChange={authorChangeHandler}
-  //         />
-  //       </Modal>
-  //     );
-  //   }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Manuel" body="This is body two" />
+        <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
   );
